69a518f4f391bda0ef1d4dfd09afcf2528d56038
PetScaramussi <peterson.silva6@fatec.sp.gov.br>
2023-08-21 01:46:16 -0300

    add client

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/products';
+import { Pagination } from './models/pagination';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'Skinet';
+  products: Product[] = [];
+  n = 1;
+
+
+  constructor(private http: HttpClient) {
+
+  }
+  //asdads
+  
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: response  => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => {
+        console.log('request completed');
+        console.log('extra statment');
+      }
+    });
+  }
+}
---
+++ client/src/app/models/products.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    id: number;
+    name: string;
+    description: string;
+    price: string;
+    pictureUrl: string;
+    productType: string;
+    productBrand: string;
+}
